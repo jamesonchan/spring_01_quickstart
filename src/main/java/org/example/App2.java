fbbc989eb4e1440c61c790b9c81d0aa343c59072
Jameson Chen <[jamesonychan@gmail.com]>
2022-12-23 00:20:01 -0500

    * applicationContext bean config

---
+++ src/main/java/org/example/App2.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App2 {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService bookService = (BookService) ctx.getBean("bookService");
+        bookService.save();
+    }
+}
