53ad22b522dba0f096a64289640438d26181ef66
Jameson Chen <[jamesonychan@gmail.com]>
2022-12-24 22:51:41 -0500

    * Setter DI

--- src/main/java/org/example/dao/impl/BookDaoImpl.java
+++ src/main/java/org/example/dao/impl/BookDaoImpl.java
@@ -3,9 +3,21 @@ package org.example.dao.impl;
 import org.example.dao.BookDao;
 
 public class BookDaoImpl implements BookDao {
+
+    private int connectionNum;
+    private String databaseName;
+
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
+
+    public void setConnectionNum(int connectionNum) {
+        this.connectionNum = connectionNum;
+    }
+
     @Override
     public void save() {
-        System.out.println("book dao save ...");
+        System.out.println("book dao save ..." + databaseName + ", " + connectionNum);
     }
 
     public void init() {
--- src/main/java/org/example/service/impl/BookServiceImpl.java
+++ src/main/java/org/example/service/impl/BookServiceImpl.java
@@ -1,6 +1,7 @@
 package org.example.service.impl;
 
 import org.example.dao.BookDao;
+import org.example.dao.UserDao;
 import org.example.service.BookService;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
@@ -8,11 +9,17 @@ import org.springframework.beans.factory.InitializingBean;
 public class BookServiceImpl implements BookService, InitializingBean, DisposableBean {
 
     private BookDao bookDao;
+    private UserDao userDao;
 
     @Override
     public void save() {
         System.out.println("book service save ...");
         bookDao.save();
+        userDao.save();
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
     }
 
     public void setBookDao(BookDao bookDao) {
