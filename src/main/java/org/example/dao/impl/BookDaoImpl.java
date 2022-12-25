3f639b3f5e676ca8509d0a3ef62c3d3038f58395
Jameson Chen <[jamesonychan@gmail.com]>
2022-12-24 23:28:02 -0500

    * Autowire

--- src/main/java/org/example/dao/impl/BookDaoImpl.java
+++ src/main/java/org/example/dao/impl/BookDaoImpl.java
@@ -7,14 +7,17 @@ public class BookDaoImpl implements BookDao {
     private int connectionNum;
     private String databaseName;
 
-    public BookDaoImpl(int connectionNum, String databaseName) {
+    public void setConnectionNum(int connectionNum) {
         this.connectionNum = connectionNum;
+    }
+
+    public void setDatabaseName(String databaseName) {
         this.databaseName = databaseName;
     }
 
     @Override
     public void save() {
-        System.out.println("book dao save ..." + databaseName + ", " + connectionNum);
+        System.out.println("book dao save ...");
     }
 
     public void init() {
--- src/main/java/org/example/service/impl/BookServiceImpl.java
+++ src/main/java/org/example/service/impl/BookServiceImpl.java
@@ -11,8 +11,11 @@ public class BookServiceImpl implements BookService, InitializingBean, Disposabl
     private BookDao bookDao;
     private UserDao userDao;
 
-    public BookServiceImpl(BookDao bookDao, UserDao userDao) {
+    public void setBookDao(BookDao bookDao) {
         this.bookDao = bookDao;
+    }
+
+    public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
     }
 
